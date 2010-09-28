a9f6c743d20cd20ad1fc6d20bfb4a78a4efb0ec9
boblu <boblu@jiaeil.com>
2010-09-28 11:07:38 +0900

    Initial project

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+
+group :development do
+	gem 'capistrano'
+end
+
+gem 'ruby-debug19', :require => 'ruby-debug'
+
+# mongoid
+gem 'mongoid', '~>2.0.0.beta'
+gem 'bson_ext'
+
+## devise
+gem 'devise'
+
+## required by devise when using mongoid orm
+gem 'mongo_ext'
+gem 'bcrypt-ruby'
+
+gem 'cancan'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cradle::Application.config.secret_token = '713d701dcf00ac672dfb94ed9c8acfe39829bd854a956f48952e756dc492bb8a383e46ed993ba48efc91f578eb312ebd67eca94723c7fce285145fbacdd12a09'
